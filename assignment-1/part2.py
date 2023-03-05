56fc50abb0612b429baef31e0f0ed57a873ad7ea
Eddy Lau <eddylau0328@gmail.com>
2023-03-05 19:34:30 +0800

    feat: add unfinished efficient frontier

--- assignment-1/part2.py
+++ assignment-1/part2.py
@@ -117,111 +117,136 @@ def calculate_global_minimum_variance_portfolio(
         weights=list(minimum_weights),
     )
 
-    results = None
-    for i in range(len(stocks)):
-        # position N+1, N+2 = array index N, N+1
-        equation = np.array([linear_matrix_inv[i][len(stocks)], linear_matrix_inv[i][len(stocks)+1]])
-        if i == 0:
-            results = equation
-        else:
-            results = np.vstack([results, equation])
+def calculate_efficient_frontier(
+    stocks: List[Stock],
+    read_history_records: Callable[[int], List[StockRecord]],
+):
+    """
+    Reference: (not all correct)
+    https://faculty.washington.edu/ezivot/econ424/portfolioTheoryMatrix.pdf
 
-    for i in range(len(stocks)):
-        respresentation = f'Stock {stocks[i].code:>4}   w_{i} = '
-        respresentation += f'{np.round(results[i][0], 4)} * avg_r_p '
-        if np.sign(results[i][1]) == 1:
-            respresentation += '+ '
-        else:
-            respresentation += '- '
-        respresentation += f'{np.abs(np.round(results[i][1], 4))}'
-        print(respresentation)
+    Use Lagrange multipliers to solve the problem
+
+    sd_p^2 = nΣi=1(w_i^2 * sd_i^2) + nΣi=1(nΣj=1(w_i * w_j * cov_i_j))
     
-    # sd_p^2 = nΣi=1(w_i^2 * sd_i^2) + nΣj=1(nΣk=1(w_j * w_k * cov_j_k))
-    # sd_p^2 = c1 + c2 * r_p + c3 * r_p^2
-    # index 0 is constant
-    # index 1 is r_p
-    # index 2 is r_p^2
-    equation = np.zeros(3, np.float64)
+    1 constraints:
+    r_p = nΣi=1(w_i * r_i)
 
-    # solve for constant
-    for i in range(len(stocks)):
-        equation[0] += results[i][1]**2 * standard_deviations[i]**2
-        for j in range(len(stocks)):
-            for k in range(len(stocks)):
-                equation[0] += results[j][1] * results[k][1] * covariance_matrix[j][k]
+    Introduce Lagrange multipliers, with λ
 
-    # solve for r_p
-    for i in range(len(stocks)):
-        equation[1] += 2 * results[i][0] * results[i][1] * standard_deviations[i]**2
-        for j in range(len(stocks)):
-            for k in range(len(stocks)):
-                equation[1] += results[j][0] * results[k][1] * covariance_matrix[j][k]
-                equation[1] += results[k][0] * results[j][1] * covariance_matrix[j][k]
+    L = sd_p^2 - λ * (nΣi=1(w_i * r_i) - r_p)
 
-    # solve for r_p^2
-    for i in range(len(stocks)):
-        equation[2] += results[i][0]**2 * standard_deviations[i]**2
-        for j in range(len(stocks)):
-            for k in range(len(stocks)):
-                equation[2] += results[j][0] * results[k][0] * covariance_matrix[j][k]
+    Differentiating L w.r.t. to each w_i:
 
-    print()
-    respresentation = ''
-    for i, variable in enumerate(['', 'r_p', 'r_p^2']):
-        if np.sign(equation[i]) == -1:
-            respresentation += ' - '
-        elif i != 0:
-            respresentation += ' + '
-        respresentation += f'{np.abs(np.round(equation[i], 4))}'
-        if variable:
-            respresentation += f' * {variable}'
+    ∂L/∂w_i = 0, i = 1 ... N
+      ∂L/∂λ = 0
 
-    respresentation = f'sd_p = sqrt({respresentation})'
-    print(respresentation)
+    Resulting to N + 1 equations:
 
-    print()
-    print('Minimum expected return: ', np.round(np.min(expected_returns),4))
-    print('Maximum expected return: ', np.round(np.max(expected_returns),4))
+    [ 2Σ r  1 ] [  w  ]    [  0  ]
+    [ r  0  0 ] [ λ_1 ]  = [ r_p ]
+    [ 1  0  0 ] [ λ_2 ]    [  1  ]
 
-    # generate random weights
-    test_data_sizes = 10000
-    test_weights = np.random.rand(test_data_sizes, len(stocks))
-    test_weights = test_weights / test_weights.sum(axis=1)[:, np.newaxis]
-    # sd_p^2 = nΣi=1(w_i^2 * sd_i^2) + nΣi=1(nΣj=1(w_i * w_j * cov_i_j))
-    test_sd_p = np.zeros(test_data_sizes, np.float64)
-    for k in range(test_data_sizes):
-        for i in range(len(stocks)):
-            for j in range(len(stocks)):
-                test_sd_p[k] += test_weights[k][i] * test_weights[k][j] * covariance_matrix[i][j]
-    test_sd_p += np.dot(test_weights**2, standard_deviations**2)
-    test_sd_p = np.sqrt(test_sd_p)
-    #  avg_r_p = nΣi=1(w_i * avg_r_i)
-    test_r_p = np.dot(test_weights, expected_returns)
+    [  w  ]   [ 2Σ r  1 ]^-1 [  0  ]
+    [ λ_1 ] = [ r  0  0 ]    [ r_p ]
+    [ λ_2 ]   [ 1  0  0 ]    [  1  ]
 
+    w = [w_1 w_2 ... w_n]
+    Σ = covariance matrix
+    r = expected returns
+
+    """
+    N = len(stocks)
+    covariance_matrix = calculate_covariance_matrix(stocks, read_history_records)
+    expected_returns = np.array([
+        calculate_expected_return(read_history_records(stock.code))
+    for stock in stocks])
+    linear_matrix = np.hstack((2 * covariance_matrix, np.transpose(np.array([expected_returns])), np.ones((N, 1), np.float64)))
+    linear_matrix = np.vstack((linear_matrix, np.concatenate((expected_returns, np.zeros(2, np.float64)))))
+    linear_matrix = np.vstack((linear_matrix, np.concatenate((np.ones(N, np.float64), np.zeros(2, np.float64)))))
+    linear_matrix_inv = np.linalg.inv(linear_matrix)
+
+    # weights in linear equation format, w = m * r_p + c
+    # weights store [m, c]
+    weight_linear_equations = np.zeros((N, 2), np.float64)
+    for i in range(N):
+        # index N = pos N + 1
+        # index N + 1 = pos N + 2
+        weight_linear_equations[i][0] = linear_matrix_inv[i][N]
+        weight_linear_equations[i][1] = linear_matrix_inv[i][N+1]
+        
+    """
+    sd_p^2 = nΣi=1(w_i^2 * sd_i^2) + nΣi=1(nΣj=1(w_i * w_j * cov_i_j))
+
+    sd_p^2 = 2 * w_1^2 * cov_1_1 + ... + 2 * w_N^2 * cov_N_N
+             + 2 * w_1 * w_2 * cov_1_2 + ... + 2 * w_N * w_N-1 * cov_N_N-1
+
+    first part:
+    2 * w_1^2 * cov_1_1
+    = 2 * cov_1_1 * (m_1 * r_p + c_1)^2
+    = 2 * cov_1_1 * (m_1^2 * r_p^2 + 2 * m_1 * c_1 * r_p + c_1^2)
+    = 2 * cov_1_1 * m_1^2 * r_p^2
+      + 4 * cov_1_1 * m_1 * c_1 * r_p
+      + 2 * cov_1_1 * c_1^2
+
+    second part:
+    2 * w_1 * w_2 * cov_1_2
+    = 2 * cov_1_2 * (m_1 * r_p + c_1) * (m_2 * r_p + c_2)
+    = 2 * cov_1_2 * [m_1 * m_2 * r_p^2 + (m_1 * c_2 + m_2 * c_1) * r_p + c_1 * c_2]
+    = 2 * cov_1_2 * m_1 * m_2 * r_p^2
+      + 2 * cov_1_2 * (m_1 * c_2 + m_2 * c_1) * r_p
+      + 2 * cov_1_2 * c_1 * c_2
+
+    Combine:
+    sd_p^2 = A * r_p^2 + B * r_p + C
+    """
+    A, B, C = 0, 0, 0
+    for i in range(N):
+        # first part
+        A += 2 * covariance_matrix[i][i] * weight_linear_equations[i][0]**2
+        B += 4 * covariance_matrix[i][i] * weight_linear_equations[i][0] * weight_linear_equations[i][1]
+        C += 2 * covariance_matrix[i][i] * weight_linear_equations[i][1]**2
+        # second part
+        for j in range(N):
+            if i == j:
+                continue
+            sub_part_a = 1
+            sub_part_a *= 2 * covariance_matrix[i][j]
+            sub_part_a *= weight_linear_equations[i][0]
+            sub_part_a *= weight_linear_equations[j][0]
+            A += sub_part_a
+            sub_part_b = 1
+            sub_part_b *= 2 * covariance_matrix[i][j]
+            sub_part_b *= (
+                weight_linear_equations[i][0] * weight_linear_equations[j][1] +
+                weight_linear_equations[j][0] * weight_linear_equations[i][0]
+            )
+            B += sub_part_b
+            sub_part_c = 1
+            sub_part_c *= 2 * covariance_matrix[i][j]
+            sub_part_c *= weight_linear_equations[i][1]
+            sub_part_c *= weight_linear_equations[j][1]
+            C += sub_part_c
+
+    print(f'sd_p^2 = {A} * r_p^2 + {B} * r_p + {C}')
     r_p = np.linspace(
-        np.min(expected_returns),
+        0,
         np.max(expected_returns),
         1000,
     )
-    sd_p = np.sqrt(equation[0] + equation[1] * r_p + equation[2] * r_p**2)
+    sd_p = np.sqrt(A * r_p**2 + B * r_p + C)
 
-    plt.plot(sd_p, r_p)
-    plt.scatter(test_sd_p, test_r_p)
-    fk = 0
-    for i in range(len(stocks)):
-        for j in range(len(stocks)):
-            fk += global_minimum_variance_weights[i] * global_minimum_variance_weights[j] * covariance_matrix[i][j]
-    fk += np.dot(global_minimum_variance_weights**2, standard_deviations**2)
-    fk = np.sqrt(fk)
-    plt.scatter(
-        # [np.dot(np.dot(global_minimum_variance_weights, covariance_matrix), global_minimum_variance_weights)],
-        [fk],
-        [np.dot(global_minimum_variance_weights, expected_returns)],
-        color='red',
-    )
-    plt.xlabel("Portfolio Standard Deviation")
-    plt.ylabel("Portfolio Return")
-    plt.show()
+
+    # plt.scatter(
+    #     # [np.dot(np.dot(global_minimum_variance_weights, covariance_matrix), global_minimum_variance_weights)],
+    #     [fk],
+    #     [np.dot(global_minimum_variance_weights, expected_returns)],
+    #     color='red',
+    # )
+    # plt.plot(sd_p, r_p)
+    # plt.xlabel("Portfolio Standard Deviation")
+    # plt.ylabel("Portfolio Return")
+    # plt.show()
 
 
 def run(
@@ -253,3 +278,5 @@ def run(
     print('Portfolio weights:')
     for i, stock in enumerate(stocks):
         print(f'Stock {stock.code:>5} weight = {global_mv_portfolio.weights[i]}')
+
+    calculate_efficient_frontier(stocks, read_history_records)
